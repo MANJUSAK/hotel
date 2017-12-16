78d2aa4c760e54f5b3e8c1b0e64d3cfc676c7064
严彬荣 <453420698@qq.com>
2017-12-16 09:02:46 +0800

    代码同步

--- src/main/java/com/goodsoft/hotel/HotelApplication.java
+++ src/main/java/com/goodsoft/hotel/HotelApplication.java
@@ -10,8 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 
 /**
  * function 系统启动程序入口
- * Created by 严彬荣 on 2017/11/7.
- * version v1.0
+ * Created by manjusaka[manjusakachn@gmail.com] on 2017/11/7.
+ *
+ * @version v1.0
  */
 @ComponentScan(basePackages = "com.goodsoft.hotel.*")
 @ServletComponentScan(basePackages = "com.goodsoft.hotel.config.*")
